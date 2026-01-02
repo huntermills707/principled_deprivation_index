32a361b7e80c33bfd5852fc3e7c8532c2611bee6
Hunter Mills <huntermills707@pm.me>
2026-01-01 22:41:16 -0800

    clean up and documentation of 01b

--- 01b_census_pull.jl
+++ 01b_census_pull.jl
@@ -10,13 +10,36 @@ include("secrets.jl");
 ENV["census_key"] = census_key;
 
 function format_ucgid(;agg="tract", state="", comp="Not a geographic component")
+    """
+    Function to get US Census query string.
+
+    # Arguments
+    - `agg::string`: How to agg geo data. EX: tract, county, ...
+    - `state::string`: What state to query. If empty string of default value, query all states.
+    - `comp::string`: What US Census component
+    # Returns
+    - `out::string`: US Census query string.
+    """
     if state != ""
         return "pseudo(04000$(geographic_components[comp])US$(states[state])\$$(geographies[agg])0000)"
     end
     return "pseudo(01000$(geographic_components[comp])US\$$(geographies[agg])0000)"
 end
 
+
 function query_census(surv, var; agg="county", state="")
+    """
+    funtion to query US Census from a specified aggregated survey variable.
+
+    # Arguments
+    - `surv::string`: US Census survey
+    - `var::string`: US Census survey variable. 
+    - `agg::string`: How to agg geo data. EX: tract, county, ...
+    - `state::sring`: What state to query. If empty string of default value, query all states.
+
+    # Returns
+    - `df::DataFrame`: Dataframe of query results.
+    """
     if var[1] == 'D'
         url = "https://api.census.gov/data/$surv/profile"
     elseif var[1] == 'S'
@@ -55,112 +78,40 @@ function query_census(surv, var; agg="county", state="")
     return DataFrame(df);
 end
 
-surv_dec = "2020/dec/dhc"
-vars_dec = [
-    # rurality
-    "Rural" => "H2_003N"
-    "Urban" => "H2_002N"
-    "Urban + Rural" => "H2_001N"
-]
 
-surv_acs = "2023/acs/acs5"
-vars_acs = [
-    "TotalPopulation" => "B01001_001E"
-    
-    # Single-Parent Households
-    "Male led Household with childeren" => "B11005_006E"
-    "Female led Household with childeren" => "B11005_007E"
-    # Nonfamilty household with child
-    "NonFamily Household with childeren" => "B11005_008E"
-    "Households with childeren" => "B11005_002E"
+function pull(group)
+    """
+    Function to pull all US Census variables defined in census_vars.jl.
+    Pulls both from ACS5 and DHC (for rurality).
 
-    # youth not in school
-    "Male 16-19 no HS" => "B14005_012E"
-    "Female 16-19 no HS" => "B14005_026E"
-    "Female + Male 16-19" => "B14005_001E"
+    # Arguments
+    - `group::string`: What group to pull (EX: tract, county, ...)
 
-    # Pay Gap Sex
-    "Male median earnings" => "B20001_002E"
-    "Female median earnings" => "B20001_023E"
+    # Returns
+    - `df::DataFrame`: DataFrame of results. Each column is a varaible, and each row is a location.
+    """
+    dfs = [
+        [query_census(surv_acs, var, agg=group)[!, ["ucgid", "NAME", var]] for (_, var) in vars_acs];
+        [query_census(surv_dec, var, agg=group)[!, ["ucgid", "NAME", var]] for (_, var) in vars_dec]
+    ]
 
-    # Pay Gap Race
-    "White median earnings" => "B20017A_001E"
-    "Black median earnings" => "B20017B_001E"
+    df = dfs[1]
+    for next_df in dfs[2:end]
+        leftjoin!(df, next_df, on=["NAME", "ucgid"])
+    end
 
-    # Housing Gap
-    "Median House Price" => "B19013_001E"
-    "Median Salary" => "B25077_001E"
-
-    "Unemployment Rate" => "S2301_C04_001E"
-    "Owner Occupied Housing" => "DP04_0046PE"
-
-    #white collar jobs
-    "White collar workers" => "C24060_002E"
-    "Total workers" => "C24060_001E"
-
-    # SNAP
-    "Total Households" => "B22002_001E"
-    "Total Households w/ SNAP" => "B22002_002E"
-    "Total Households w/ SNAP + childeren" => "B22002_003E"
-    "Total Households w/ SNAP + elders" => "B22001_003E"
-
-    #tech
-    "Households w/o Internet" => "B28002_013E"
-    "Households w/ Broadband Internet" => "B28002_004E"
-    "Households w/ Smartphone" => "B28010_005E"
-
-    # char
-    "Households w/o Car" => "DP04_0058E"
-    "Households w/o Complete Plumbing" => "DP04_0073PE"
-    "Households w/o Complete Kitchen" => "DP04_0074E"
-    
-    # transport
-    "Over 60 min commuting" => "S0801_C01_045E"
-    "WFH" => "S0801_C01_013E"
-    "Public Transit to Work" => "S0801_C01_009E"
-    "Walked to Work" => "S0801_C01_010E"
-    "Biked to Work" => "S0801_C01_011E"
-    "Total Workers" => "S0801_C01_001E"
-];
-
-# US Census County
-dfs = [
-    [query_census(surv_acs, var, agg="county")[!, ["ucgid", "NAME", var]] for (_, var) in vars_acs];
-    [query_census(surv_dec, var, agg="county")[!, ["ucgid", "NAME", var]] for (_, var) in vars_dec]
-]
-
-df = dfs[1]
-for next_df in dfs[2:end]
-    leftjoin!(df, next_df, on=["NAME", "ucgid"])
+    return df
 end
 
+# Pull census data
+df = pull("county")
 fname = "raw_data/census/census_data_county_raw.csv"
 CSV.write(fname, df)
 
-# US Census D ZCTA
-dfs = [
-    [query_census(surv_acs, var, agg="zip code tabulation area")[!, ["ucgid", "NAME", var]] for (_, var) in vars_acs];
-    [query_census(surv_dec, var, agg="zip code tabulation area")[!, ["ucgid", "NAME", var]] for (_, var) in vars_dec]
-]
-
-df = dfs[1]
-for next_df in dfs[2:end]
-    leftjoin!(df, next_df, on=["NAME", "ucgid"])
-end
-
+df = pull("zip code tabulation area")
 fname = "raw_data/census/census_data_zcta_raw.csv"
 CSV.write(fname, df)
 
-# US Census TRACTS
-dfs = [
-    [query_census(surv_acs, var, agg="tract")[!, ["ucgid", "NAME", var]] for (_, var) in vars_acs];
-    [query_census(surv_dec, var, agg="tract")[!, ["ucgid", "NAME", var]] for (_, var) in vars_dec]
-]
-
-df = dfs[1]
-for next_df in dfs[2:end]
-    leftjoin!(df, next_df, on=["NAME", "ucgid"])
-end
-
+df = pull("tract")
 fname = "raw_data/census/census_data_tract_raw.csv"
 CSV.write(fname, df)
