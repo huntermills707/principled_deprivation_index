90477490bac6cce8544d197316b444217c218e88
Hunter Mills <huntermills707@pm.me>
2026-01-22 22:15:40 -0800

    dubugging 02b

--- 02b_fema_eal.jl
+++ 02b_fema_eal.jl
@@ -4,7 +4,6 @@ using DataFrames
 geog = DataFrame(CSV.File("derived_data/geography.csv"))
 
 #https://hazards.fema.gov/nri/expected-annual-loss
-
 df_nri_county = DataFrame(CSV.File("raw_data/fema_eal/NRI_Table_Counties/NRI_Table_Counties.csv"))
 
 nri_data_county = df_nri_county[:, [
@@ -15,30 +14,34 @@ nri_data_county = df_nri_county[:, [
 rename!(nri_data_county, [
     (:STATEFIPS => :STATE),
     (:COUNTY => :COUNTYNAME),
-    (:STCOPIPS => :COUNTY),
+    (:STCOFIPS => :COUNTY),
 ])
 
 transform!(nri_data_county, :COUNTYNAME => ByRow(x -> x === missing ? missing : lowercase(x)) => :COUNTYNAME)
 
+#https://hazards.fema.gov/nri/expected-annual-loss
 df_nri_tract = DataFrame(CSV.File("raw_data/fema_eal/NRI_Table_CensusTracts/NRI_Table_CensusTracts.csv"))
 
-nri_data_tract = df_nri_tract[: [
+nri_data_tract = df_nri_tract[:, [
     :TRACTFIPS, :ALR_VALB, :ALR_VALP, :ALR_VALA
 ]]
 
 rename!(nri_data_tract, (:TRACTFIPS => :TRACT))
 
-nri_data_zip = df_nri_tract[[
+nri_data_zip = df_nri_tract[:, [
     :TRACTFIPS,
     :ALR_VALB, :ALR_VALP, :ALR_VALA,
     :EAL_VALB, :EAL_VALP, :EAL_VALA,
     :POPULATION, :BUILDVALUE, :AGRIVALUE
 ]]
 
-
 rename!(nri_data_zip, (:TRACTFIPS => :TRACT))
 
-nri_data_zip = innerjoin(nri_data_zip, geog, on=:Tract)
+nri_data_zip = innerjoin(nri_data_zip, geog, on=:TRACT)
+
+transform!(nri_data_zip, :POPULATION => ByRow(x -> Float64(x)) => :POPULATION)
+transform!(nri_data_zip, :BUILDVALUE => ByRow(x -> Float64(x)) => :BUILDVALUE)
+transform!(nri_data_zip, :AGRIVALUE => ByRow(x -> Float64(x)) => :AGRIVALUE)
 
 nri_data_zip[:, :EAL_VALB] .*= nri_data_zip[:, :TZ_RATIO]
 nri_data_zip[:, :EAL_VALP] .*= nri_data_zip[:, :TZ_RATIO]
@@ -47,17 +50,17 @@ nri_data_zip[:, :POPULATION] .*= nri_data_zip[:, :TZ_RATIO]
 nri_data_zip[:, :BUILDVALUE] .*= nri_data_zip[:, :TZ_RATIO]
 nri_data_zip[:, :AGRIVALUE] .*= nri_data_zip[:, :TZ_RATIO]
 
-function ratio(sub_df, :col1, :col2)
+function ratio(sub_df, col1, col2)
     # Keep only rows where BOTH columns are non‑missing
-    clean = dropmissing(sub_df, [:col1, :col2])
+    clean = dropmissing(sub_df, [col1, col2])
 
     # If everything got dropped, avoid division‑by‑zero
     if nrow(clean) == 0
         return missing
     end
 
-    s1 = sum(skipmissing(clean.col1))
-    s2 = sum(skipmissing(clean.col2))
+    s1 = sum(skipmissing(clean[:, col1]))
+    s2 = sum(skipmissing(clean[:, col2]))
     return s1 / s2
 end
 
@@ -67,7 +70,11 @@ nri_data_zip = combine(groupby(nri_data_zip, :ZIP)) do sub_df
      ALR_VALA = ratio(sub_df, :EAL_VALA, :AGRIVALUE))
 end
 
-CSV.write(nri_data_county, "derived_data/county/fema_eal.csv")
-CSV.write(nri_data_zip, "derived_data/zcta/fema_eal.csv")
-CSV.write(nri_data_tract, "derived_data/tract/fema_eal.csv")
+mkpath("derived_data/county")
+mkpath("derived_data/zcta")
+mkpath("derived_data/tract")
+
+CSV.write("derived_data/county/fema_eal.csv", nri_data_county)
+CSV.write("derived_data/zcta/fema_eal.csv", nri_data_zip)
+CSV.write("derived_data/tract/fema_eal.csv", nri_data_tract)
 
